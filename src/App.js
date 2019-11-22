ce85cded3cbf32fcbb9f25e48eb1409b533b0ab8
terry <terry@trustgrid.io>
2019-11-22 17:45:56 +1300

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import pgp from 'pg-promise';
+
+const config = {
+  host: 'localhost',
+  port: 5432,
+  database: 'finance_funday',
+  user: 'tinle',
+  password: '',
+  max: 5, // max number of clients in the pool
+  idleTimeoutMillis: 1000, // how long a client is allowed to remain idle before being closed
+};
+
+
+const db = pgp(config);
+
+
+//const { db } = require('../connections/redshift');
+
+db.any('SELECT * FROM product WHERE price BETWEEN $1 AND $2', [1, 10])
+
 
 function App() {
   return (
@@ -8,7 +28,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          xEdit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
